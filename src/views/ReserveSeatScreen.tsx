47b464d92b436f9f3fa6608df9bb711458ba61b2
michaell027 <miselka12345@gmail.com>
2023-10-24 23:34:51 +0200

    m

--- src/views/ReserveSeatScreen.tsx
+++ src/views/ReserveSeatScreen.tsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, StyleSheet, Platform } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, Platform, Pressable } from 'react-native';
 import { firebase } from '@react-native-firebase/database';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCircle, faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 function ReserveSeatScreen({ navigation, route }) {
     const [seats, setSeats] = useState([]);
     const [date, setDate] = useState(new Date());
-    const [mode, setMode] = useState<'date' | 'time'>('date');
     const [show, setShow] = useState(false);
+    const [showTimes, setShowTimes] = useState(false);
+    const [selectedSeatId, setSelectedSeatId] = useState(null);
+    const [selectedTimes, setSelectedTimes] = useState([]);
     const { restaurantId } = route.params;
+    const [selectView, setSelectView] = useState<'date' | 'table'>('date');
 
     const reference = firebase
         .app()
         .database('https://sitnserve-fbaed-default-rtdb.europe-west1.firebasedatabase.app/')
-        .ref(`/restaurant_id/${restaurantId}/seats`);
+        .ref(`/restaurant_id/${restaurantId}/tables`);
 
     useEffect(() => {
         const unsubscribe = reference.on('value', snapshot => {
@@ -36,20 +41,65 @@ function ReserveSeatScreen({ navigation, route }) {
         };
     }, []);
 
+    const handleTimeSelect = (time) => {
+        if (selectedTimes.includes(time)) {
+            setSelectedTimes(selectedTimes.filter(t => t !== time));
+        } else {
+            setSelectedTimes([...selectedTimes, time]);
+        }
+    };
+
+    const renderTimes = (id) => {
+        const seat = seats.find(seat => seat.id === id);
+        const dateString = date.toISOString().split('T')[0];
+        const times = seat?.reserved[dateString];
+
+        if (!seat) {
+            return null;
+        }
+
+        return (
+            <View>
+                {times && Object.keys(times)
+                    .sort((a, b) => a.localeCompare(b))
+                    .map(time => (
+                        <Pressable
+                            key={time}
+                            style={styles.button}
+                            onPress={() => {
+                        if (!times[time].occupied.value) {
+                            console.log(time);
+                            handleTimeSelect(time);
+                        }
+                    }}
+                            disabled={times[time].occupied.value}
+                        >
+                    {selectedTimes.includes(time) ? (
+                  <FontAwesomeIcon icon={faCircleCheck} />
+                ) : (
+                  <FontAwesomeIcon icon={faCircle} />
+                )}
+                <Text style={styles.text}>{time}</Text>
+              </Pressable>
+            ))}
+        </View>
+      );
+    };
+
     const renderItem = ({ item, index }) => (
         <View style={styles.container}>
             <Text style={styles.text}>
-                ID: {item.id} Seat: {item.seat}
+                ID: {item.id} Seats: {item.seats}
             </Text>
-            <Button
-                title={item.occupied ? 'Reserved' : 'Select'}
+            <Pressable
+                className={`bg-blue-500 p-2 rounded-md`}
                 onPress={() => {
-                    if (!item.occupied) {
-                        reference.child(item.id.toString()).update({ occupied: true });
-                    }
-                }}
-                disabled={item.occupied}
-            />
+                    setSelectedSeatId(prevId => (prevId === item.id ? null : item.id));
+                    setSelectedTimes([]);
+                }}>
+                <Text>Select time</Text>
+            </Pressable>
+            {selectedSeatId === item.id && renderTimes(item.id)}
         </View>
     );
 
@@ -57,41 +107,55 @@ function ReserveSeatScreen({ navigation, route }) {
         const currentDate = selectedDate || date;
         setShow(Platform.OS === 'ios');
         setDate(currentDate);
-    };
-
-    const showMode = (currentMode) => {
-        setShow(true);
-        setMode(currentMode);
+        setSelectedSeatId(null);
+        setSelectedTimes([]);
     };
 
     const showDatePicker = () => {
-        showMode('date');
-    };
-
-    const showTimePicker = () => {
-        showMode('time');
+        setShow(true);
     };
 
     return (
         <View style={styles.screen}>
+            <View className={`flex flex-row w-full`}>
+                <Pressable
+                    onPress={() => setSelectView('date')}
+                    className={`${selectView === 'date' ? 'border-b-2 border-blue-500' : ''} w-1/2 items-center`}
+                >
+                    <Text className={`${selectView === 'date' ? 'text-blue-500' : ''} text-lg`}>Select date</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => setSelectView('table')}
+                    className={`${selectView === 'table' ? 'border-b-2 border-blue-500' : ''} w-1/2 items-center`}
+                >
+                    <Text className={`${selectView === 'table' ? 'text-blue-500' : ''} text-lg`}>Select table</Text>
+                </Pressable>
+            </View>
+            {selectView === 'date' && (
+                <View style={styles.datePicker}>
+                    <Text>{date.toISOString().split('T')[0]}</Text>
+                </View>
+            )}
             <View>
                 <Button onPress={showDatePicker} title='Show date picker' />
-                <Button onPress={showTimePicker} title='Show time picker' />
                 {show && (
                     <DateTimePicker
                         testID='dateTimePicker'
                         value={date}
-                        mode={mode}
+                        mode='date'
                         is24Hour={true}
                         display='default'
                         onChange={onChange}
+                        minimumDate={new Date()}
+                        maximumDate={new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)}
                     />
+
                 )}
             </View>
             <FlatList
                 data={seats}
                 renderItem={renderItem}
-                keyExtractor={(item) => item.seat.toString()}
+                keyExtractor={(item) => item.id.toString()}
             />
         </View>
     );
