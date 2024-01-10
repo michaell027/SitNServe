cbe724448b04cf61fa8d6a3c8d9b06ef321c9b8d
michaell027 <miselka12345@gmail.com>
2024-01-10 21:58:01 +0100

    order model

---
+++ src/models/Order.ts
@@ -0,0 +1,17 @@
+interface OrderItem {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+export interface Order {
+    id: string;
+    restaurantId: string;
+    restaurantName: string;
+    restaurantImage: string;
+    seat: string;
+    total: number;
+    date: Date;
+    items: OrderItem[];
+}
